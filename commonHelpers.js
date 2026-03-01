c9d3b43cf2478c9795af0eab2bf049db8e24e405
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2026-03-01 08:36:11 +0000

    Deploying to gh-pages from @ Nazar0929/musicbooster@3aeab2e9d86b15a89bb3f56a0d1154bee0ce8b96 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const c of a.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function n(e){if(e.ep)return;e.ep=!0;const a=o(e);fetch(e.href,a)}})();const q="Q8bHL81HES4CjxatVZAVSQWYyAffYhbQ",H=document.querySelector(".events-list"),y=document.getElementById("pagination"),b=document.querySelector("[data-modal]"),j=document.querySelector(".modal__wrap"),x=document.querySelector("[data-close]"),N=document.body;let g=0,u=0;async function v(t=0){var a,c;const s=`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${q}&countryCode=US&size=20&page=${t}`,n=await(await fetch(s)).json(),e=((a=n._embedded)==null?void 0:a.events)||[];u=Math.min(((c=n.page)==null?void 0:c.totalPages)||0,29),g=t,T(e),W()}function T(t){const s=t.map(o=>{var p,f,i,r,d,l,m;const n=((f=(p=o.images)==null?void 0:p[0])==null?void 0:f.url)||"",e=o.name||"",a=((r=(i=o.dates)==null?void 0:i.start)==null?void 0:r.localDate)||"",c=((m=(l=(d=o._embedded)==null?void 0:d.venues)==null?void 0:l[0])==null?void 0:m.name)||"";return`
-      <li class="event-card" data-id="${o.id}">
-        <img class="event-img" src="${n}" alt="${e}">
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function a(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(e){if(e.ep)return;e.ep=!0;const o=a(e);fetch(e.href,o)}})();const N="Q8bHL81HES4CjxatVZAVSQWYyAffYhbQ",I=document.querySelector(".events-list"),g=document.getElementById("pagination"),M=document.querySelector("[data-modal]"),q=document.querySelector(".modal__wrap"),B=document.querySelector("[data-close]"),O=document.body;let b=0,v=0;async function E(t=0){var o,c;const n=`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${N}&countryCode=US&size=20&page=${t}`,s=await(await fetch(n)).json(),e=((o=s._embedded)==null?void 0:o.events)||[];v=Math.min(((c=s.page)==null?void 0:c.totalPages)||0,29),b=t,Y(e),U()}function Y(t){const n=t.map(a=>{var i,d,l,r,m,p,u;const s=((d=(i=a.images)==null?void 0:i[0])==null?void 0:d.url)||"",e=a.name||"",o=((r=(l=a.dates)==null?void 0:l.start)==null?void 0:r.localDate)||"",c=((u=(p=(m=a._embedded)==null?void 0:m.venues)==null?void 0:p[0])==null?void 0:u.name)||"";return`
+      <li class="event-card" data-id="${a.id}">
+        <img class="event-img" src="${s}" alt="${e}">
         <h3 class="event-title">${e}</h3>
-        <p class="event-date">${a}</p>
+        <p class="event-date">${o}</p>
         <p class="event-place">${c}</p>
       </li>
-    `}).join("");H.innerHTML=s}function W(){y.innerHTML="";const t=5;let s=Math.max(0,g-2),o=Math.min(u,s+t);o-s<t&&(s=Math.max(0,o-t));for(let n=s;n<o;n++){const e=document.createElement("button");e.className="page-btn",e.textContent=n+1,n===g&&e.classList.add("active"),e.addEventListener("click",()=>v(n)),y.appendChild(e)}if(o<u){const n=document.createElement("span");n.textContent="...",n.className="dots",y.appendChild(n);const e=document.createElement("button");e.className="page-btn",e.textContent=u,e.addEventListener("click",()=>v(u-1)),y.appendChild(e)}}H.addEventListener("click",A);async function A(t){const s=t.target.closest(".event-card");if(!s)return;const o=s.dataset.id,e=await(await fetch(`https://app.ticketmaster.com/discovery/v2/events/${o}.json?apikey=${q}`)).json();j.innerHTML=I(e),b.classList.remove("backdrop-hidden"),N.classList.add("no-scroll")}function I(t){var i,r,d,l,m,$,k,M,_,S,w,C,P;const s=((r=(i=t.images)==null?void 0:i[0])==null?void 0:r.url)||"",o=t.name||"",n=t.info||"No information",e=((l=(d=t.dates)==null?void 0:d.start)==null?void 0:l.localDate)||"",a=(($=(m=t.dates)==null?void 0:m.start)==null?void 0:$.localTime)||"",c=((S=(_=(M=(k=t._embedded)==null?void 0:k.venues)==null?void 0:M[0])==null?void 0:_.city)==null?void 0:S.name)||"",p=((P=(C=(w=t._embedded)==null?void 0:w.venues)==null?void 0:C[0])==null?void 0:P.name)||"",f=t.url||"#";return`
-    <img class="modal__preview" src="${s}" />
+    `}).join("");I.innerHTML=n}function U(){g.innerHTML="";const t=5;let n=Math.max(0,b-2),a=Math.min(v,n+t);a-n<t&&(n=Math.max(0,a-t));for(let s=n;s<a;s++){const e=document.createElement("button");e.className="page-btn",e.textContent=s+1,s===b&&e.classList.add("active"),e.addEventListener("click",()=>E(s)),g.appendChild(e)}if(a<v){const s=document.createElement("span");s.textContent="...",s.className="dots",g.appendChild(s);const e=document.createElement("button");e.className="page-btn",e.textContent=v,e.addEventListener("click",()=>E(v-1)),g.appendChild(e)}}I.addEventListener("click",V);async function V(t){const n=t.target.closest(".event-card");if(!n)return;const a=n.dataset.id,e=await(await fetch(`https://app.ticketmaster.com/discovery/v2/events/${a}.json?apikey=${N}`)).json();q.innerHTML=K(e),M.classList.remove("backdrop-hidden"),O.classList.add("no-scroll")}function K(t){var l,r,m,p,u,h,f,x,S,C,z,P,T;const n=((r=(l=t.images)==null?void 0:l[0])==null?void 0:r.url)||"",a=t.name||"",s=t.info||"No information",e=((p=(m=t.dates)==null?void 0:m.start)==null?void 0:p.localDate)||"",o=((h=(u=t.dates)==null?void 0:u.start)==null?void 0:h.localTime)||"",c=((C=(S=(x=(f=t._embedded)==null?void 0:f.venues)==null?void 0:x[0])==null?void 0:S.city)==null?void 0:C.name)||"",i=((T=(P=(z=t._embedded)==null?void 0:z.venues)==null?void 0:P[0])==null?void 0:T.name)||"",d=t.url||"#";return`
+    <img class="modal__preview" src="${n}" />
 
     <div class="content">
 
-      <img class="content__image" src="${s}" />
+      <img class="content__image" src="${n}" />
 
       <ul>
 
-        <li><b>INFO:</b> ${n}</li>
-        <li><b>WHEN:</b> ${e} ${a}</li>
-        <li><b>WHERE:</b> ${c} — ${p}</li>
-        <li><b>WHO:</b> ${o}</li>
+        <li><b>INFO:</b> ${s}</li>
+        <li><b>WHEN:</b> ${e} ${o}</li>
+        <li><b>WHERE:</b> ${c} — ${i}</li>
+        <li><b>WHO:</b> ${a}</li>
 
         <li>
-          <a class="modal__btn" href="${f}" target="_blank">
+          <a class="modal__btn" href="${d}" target="_blank">
             BUY TICKETS
           </a>
         </li>
@@ -28,5 +28,72 @@
       </ul>
 
     </div>
-  `}x.addEventListener("click",L);b.addEventListener("click",t=>{t.target.closest(".modal")||L()});document.addEventListener("keydown",t=>{t.code==="Escape"&&L()});function L(){b.classList.add("backdrop-hidden"),N.classList.remove("no-scroll")}v();const O=document.body,h=document.querySelector("[data-modal]"),B=document.querySelector("[data-close]"),Y=document.querySelector(".js-events-gallery"),D=document.querySelector(".modal__wrap");Y.addEventListener("click",F);function F(t){if(!t.target.closest("[data-id]"))return;const o=K();D.innerHTML=createMarkup(o),h.classList.remove("backdrop-hidden"),O.classList.add("no-scroll")}B.addEventListener("click",E);h.addEventListener("click",t=>{t.target.closest(".modal")||E()});document.addEventListener("keydown",t=>{t.code==="Escape"&&E()});function E(){h.classList.add("backdrop-hidden"),O.classList.remove("no-scroll")}function K(){return{name:"Atlas Weekend",info:"Largest music festival in Ukraine.",date:"2026-02-28",time:"20:00",city:"Jarosław, Poland",place:"Reto Music Club",price:"Standard 40-80€",img:"https://picsum.photos/400/600"}}
+  `}B.addEventListener("click",H);M.addEventListener("click",t=>{t.target.closest(".modal")||H()});document.addEventListener("keydown",t=>{t.code==="Escape"&&H()});function H(){M.classList.add("backdrop-hidden"),O.classList.remove("no-scroll")}E();const j="Q8bHL81HES4CjxatVZAVSQWYyAffYhbQ",A=document.querySelector(".events-list"),$=document.getElementById("pagination"),k=document.querySelector("[data-modal]"),Q=document.querySelector(".modal__wrap"),R=document.querySelector("[data-close]"),W=document.body;let y=0,_=0;async function L(t=0){var o,c;const n=`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${j}&countryCode=US&size=20&page=${t}`,s=await(await fetch(n)).json(),e=((o=s._embedded)==null?void 0:o.events)||[];_=Math.min(((c=s.page)==null?void 0:c.totalPages)||0,29),y=t,D(e),F()}function D(t){const n=t.map(a=>{var s,e,o,c,i,d,l;return`
+    <li class="event-card" data-id="${a.id}">
+      <img class="event-img" src="${((e=(s=a.images)==null?void 0:s[0])==null?void 0:e.url)||""}" alt="${a.name}">
+      <h3 class="event-title">${a.name}</h3>
+      <p class="event-date">${((c=(o=a.dates)==null?void 0:o.start)==null?void 0:c.localDate)||""}</p>
+      <p class="event-place">${((l=(d=(i=a._embedded)==null?void 0:i.venues)==null?void 0:d[0])==null?void 0:l.name)||""}</p>
+    </li>
+  `}).join("");A.innerHTML=n}function F(){$.innerHTML="";const t=5;let n=Math.max(0,y-2),a=Math.min(_,n+t);a-n<t&&(n=Math.max(0,a-t));for(let s=n;s<a;s++){const e=document.createElement("button");e.className="page-btn",e.textContent=s+1,s===y&&e.classList.add("active"),e.addEventListener("click",()=>L(s)),$.appendChild(e)}if(a<_){const s=document.createElement("span");s.textContent="...",s.className="dots",$.appendChild(s);const e=document.createElement("button");e.className="page-btn",e.textContent=_,e.addEventListener("click",()=>L(_-1)),$.appendChild(e)}}A.addEventListener("click",async t=>{const n=t.target.closest(".event-card");if(!n)return;const a=n.dataset.id,e=await(await fetch(`https://app.ticketmaster.com/discovery/v2/events/${a}.json?apikey=${j}`)).json();Q.innerHTML=Z(e),k.classList.remove("backdrop-hidden"),W.classList.add("no-scroll")});R.addEventListener("click",w);k.addEventListener("click",t=>{t.target.closest(".modal")||w()});document.addEventListener("keydown",t=>{t.code==="Escape"&&w()});function w(){k.classList.add("backdrop-hidden"),W.classList.remove("no-scroll")}function Z(t){var n,a,s,e,o,c,i,d,l,r,m,p,u,h,f;return`
+    <img class="modal__preview" src="${((a=(n=t.images)==null?void 0:n[0])==null?void 0:a.url)||""}" />
+
+    <div class="content">
+      <img class="content__image" src="${((e=(s=t.images)==null?void 0:s[0])==null?void 0:e.url)||""}" />
+
+      <ul class="content__list">
+
+        <li>
+          <h2 class="modal__title">INFO</h2>
+          <p class="modal__text">${t.info||"No information"}</p>
+        </li>
+
+        <li>
+          <h2 class="modal__title">WHEN</h2>
+          <p class="modal__text">${((c=(o=t.dates)==null?void 0:o.start)==null?void 0:c.localDate)||""}</p>
+          <p class="modal__text">${((d=(i=t.dates)==null?void 0:i.start)==null?void 0:d.localTime)||""}</p>
+        </li>
+
+        <li>
+          <h2 class="modal__title">WHERE</h2>
+          <p class="modal__text">${((p=(m=(r=(l=t._embedded)==null?void 0:l.venues)==null?void 0:r[0])==null?void 0:m.city)==null?void 0:p.name)||""}</p>
+          <p class="modal__text">${((f=(h=(u=t._embedded)==null?void 0:u.venues)==null?void 0:h[0])==null?void 0:f.name)||""}</p>
+        </li>
+
+        <li>
+          <h2 class="modal__title">WHO</h2>
+          <p class="modal__text">${t.name||""}</p>
+        </li>
+
+        <li>
+          <h2 class="modal__title">PRICES</h2>
+
+          <div class="price__wrap">
+          <svg class="price__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 29 20"><path d="M3.22 0H0v19.33h3.22zm8.11 0H8.1v19.33h3.23zm4.88 0h-3.22v19.33h3.22zM29 0h-4.78v19.33H29zM6.44 0H4.88v19.33h1.56zm12.89 0h-1.56v19.33h1.56zm3.23 0h-1.57v19.33h1.57z" fill="#0E0E0E"/></svg>
+            <p class="modal__text">Standart 300-500 UAH</p>
+            
+          </div>
+
+          <a class="modal__btn" href="${t.url||"#"}" target="_blank">
+            BUY TICKETS
+          </a>
+
+          <div class="price__wrap">
+            <svg class="price__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 29 20"><path d="M3.22 0H0v19.33h3.22zm8.11 0H8.1v19.33h3.23zm4.88 0h-3.22v19.33h3.22zM29 0h-4.78v19.33H29zM6.44 0H4.88v19.33h1.56zm12.89 0h-1.56v19.33h1.56zm3.23 0h-1.57v19.33h1.57z" fill="#0E0E0E"/></svg>
+            <p class="modal__text">VIP 1000-1500 UAH</p>
+          </div>
+
+          <a class="modal__btn" href="${t.url||"#"}" target="_blank">
+            BUY TICKETS
+          </a>
+
+        </li>
+
+      </ul>
+    </div>
+
+    <a class="btn-info" href="${t.url||"#"}" target="_blank">
+      MORE FROM THIS AUTHOR
+    </a>
+  `}L();
 //# sourceMappingURL=commonHelpers.js.map
